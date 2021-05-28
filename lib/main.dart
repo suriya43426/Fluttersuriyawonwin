2e7379126d25c91c7f2fb7c114930c51ba052654
suriya chayatummagoon <6171006221@student.chula.ac.th>
2021-05-28 14:59:58 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:designcode/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,12 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Basic APP'),),
-        body: Center(
-          child: Text('Suriya Chyatummagoon'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
-}
