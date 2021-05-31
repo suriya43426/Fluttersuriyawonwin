091bf6af5081ff35c86e9c899242e43d94499479
suriya chayatummagoon <6171006221@student.chula.ac.th>
2021-05-31 12:16:12 +0700

    HomeScreenNavBar

--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,67 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SafeArea(
-            child: Container(
-          child: ResentCourseCard(
-            course: recentCourses[0],
+        body: Container(
+          color: kBackgroundColor,
+          child: SafeArea(
+            child: Column(
+              children: [
+                HomeScreenNavBar(),
+                ResentCourseCard(
+                  course: recentCourses[0],
+                ),
+              ],
+            ),
           ),
-        )),
+        ),
       ),
     );
   }
 }
 
+class HomeScreenNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          RawMaterialButton(
+            onPressed: () {
+              print('Sidebar button pressed');
+            },
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            highlightColor: Colors.transparent,
+            splashColor: Colors.transparent,
+            constraints: BoxConstraints(
+              maxHeight: 40.0,
+              maxWidth: 40.0
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(14.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: kShadowColor,
+                    offset: Offset(0,12),
+                    blurRadius: 16.0
+                  )
+                ],
+              ),
+              child: Image.asset(
+                'assests/icons/icon-sidebar.png',
+                color: kPrimaryLabelColor,
+              ),
+              padding: EdgeInsets.symmetric(
+                horizontal: 12.0,
+                vertical: 14.0
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
