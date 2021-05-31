33f73f148b95b0661c3f694e01f5aa4dd2c7c4a1
suriya chayatummagoon <6171006221@student.chula.ac.th>
2021-05-31 17:10:39 +0700

    RecentCourseCard, StatefulWidget, PageView.builder, Opacity

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:suriyawonwin/model/course.dart';
-import 'package:suriyawonwin/model/sidebar.dart';
 import 'components/cards/recent_course_card.dart';
-import 'components/sidebar_row.dart';
 import 'constants.dart';
 
 void main() {
@@ -22,14 +20,95 @@ class MyApp extends StatelessWidget {
             child: Column(
               children: [
                 HomeScreenNavBar(),
-                ResentCourseCard(
-                  course: recentCourses[0],
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        'Recents',
+                        style: kLargeTitleStyle,
+                      ),
+                      SizedBox(
+                        height: 5.0,
+                      ),
+                      Text(
+                        '23 Course, more coming',
+                        style: kSubtitleStyle,
+                      )
+                    ],
+                  ),
                 ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                RecentCourseList()
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class RecentCourseList extends StatefulWidget {
+  @override
+  _RecentCourseListState createState() => _RecentCourseListState();
+}
+
+class _RecentCourseListState extends State<RecentCourseList> {
+  List<Container> indicators = [];
+  int currentPage = 0;
+
+  Widget updateIndicators() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: recentCourses.map(
+        (course) {
+          var index = recentCourses.indexOf(course);
+          return Container(
+            width: 7.0,
+            height: 7.0,
+            margin: EdgeInsets.symmetric(horizontal: 6.0),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color:
+                  currentPage == index ? Color(0xFF0971FE) : Color(0xFFA6AEBD),
+            ),
+          );
+        },
+      ).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 320,
+          width: double.infinity,
+          child: PageView.builder(
+            itemBuilder: (context, index) {
+              return Opacity(
+                opacity: currentPage == index? 1.0 : 0.5,
+                child: ResentCourseCard(
+                  course: recentCourses[index],
+                ),
+              );
+            },
+            itemCount: recentCourses.length,
+            controller: PageController(initialPage: 0, viewportFraction: 0.63),
+            onPageChanged: (index){
+              setState(() {
+                currentPage = index;
+              });
+            },
+          ),
+        ),
+        updateIndicators(),
+      ],
     );
   }
 }
@@ -42,40 +121,98 @@ class HomeScreenNavBar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          RawMaterialButton(
-            onPressed: () {
-              print('Sidebar button pressed');
-            },
-            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-            highlightColor: Colors.transparent,
-            splashColor: Colors.transparent,
-            constraints: BoxConstraints(
-              maxHeight: 40.0,
-              maxWidth: 40.0
-            ),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(14.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: kShadowColor,
-                    offset: Offset(0,12),
-                    blurRadius: 16.0
-                  )
-                ],
+          SidebarButton(),
+          SearchFieldWidget(),
+          Icon(
+            Icons.notifications,
+            color: kPrimaryLabelColor,
+          ),
+          SizedBox(
+            width: 16.0,
+          ),
+          CircleAvatar(
+            radius: 18.0,
+            backgroundImage: AssetImage('assests/images/profile_ohm.png'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SidebarButton extends StatelessWidget {
+  const SidebarButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () {
+        print('Sidebar button pressed');
+      },
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      highlightColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      constraints: BoxConstraints(maxHeight: 40.0, maxWidth: 40.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14.0),
+          boxShadow: [
+            BoxShadow(
+                color: kShadowColor, offset: Offset(0, 12), blurRadius: 16.0)
+          ],
+        ),
+        child: Image.asset(
+          'assests/icons/icon-sidebar.png',
+          color: kPrimaryLabelColor,
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 14.0),
+      ),
+    );
+  }
+}
+
+class SearchFieldWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.only(left: 12.0, right: 33.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(14.0),
+            boxShadow: [
+              BoxShadow(
+                color: kShadowColor,
+                offset: Offset(0, 12),
+                blurRadius: 16.0,
+              )
+            ],
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextField(
+              cursorColor: kPrimaryLabelColor,
+              decoration: InputDecoration(
+                icon: Icon(
+                  Icons.search,
+                  color: kPrimaryLabelColor,
+                  size: 20.0,
+                ),
+                border: InputBorder.none,
+                hintText: "Search for location",
+                hintStyle: kSearchPlaceholderStyle,
               ),
-              child: Image.asset(
-                'assests/icons/icon-sidebar.png',
-                color: kPrimaryLabelColor,
-              ),
-              padding: EdgeInsets.symmetric(
-                horizontal: 12.0,
-                vertical: 14.0
-              ),
+              style: kSearchTextStyle,
+              onChanged: (newText) {
+                print(newText);
+              },
             ),
           ),
-        ],
+        ),
       ),
     );
   }
